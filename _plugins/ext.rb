227e307941c82d697747516289a1b200b368caa8
Mateusz Książek <ksiazek.mate@gmail.com>
2017-04-11 23:02:28 +0200

    Add blog post

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,34 +1,34 @@
 module Jekyll
-    module Tags
-        class TagGenerator < Generator
-            def generate(site)
-                dir_name = "_tags";
-                Dir.mkdir(dir_name) unless File.exists?(dir_name)
+  module Tags
+    class TagGenerator < Generator
+      def generate(site)
+        dir_name = "_tags";
+        Dir.mkdir(dir_name) unless File.exists?(dir_name)
 
-                for tag in site.tags
-                    tag_name = tag[0]
-                    File.open(dir_name + "/#{tag_name}.md", "w") do |f|
-                        f.puts(file_content(tag_name))
-                    end
-                end
-            end
-
-            private
-
-            def humanize_tag(tag)
-                tag.gsub("-", " ")
-            end
-
-            def file_content(tag)
-                <<~HEREDOC
-                    ---
-                    permalink: /tag/#{tag}.html
-                    layout: page_tag
-                    tag: '#{tag}'
-                    title: #{humanize_tag(tag)}
-                    ---
-                HEREDOC
-            end
+        for tag in site.tags
+          tag_name = tag[0]
+          File.open(dir_name + "/#{tag_name}.md", "w") do |f|
+            f.puts(file_content(tag_name))
+          end
         end
+      end
+
+      private
+
+      def humanize_tag(tag)
+        tag.gsub("-", " ")
+      end
+
+      def file_content(tag)
+        <<~HEREDOC
+          ---
+          permalink: /tag/#{tag}.html
+          layout: page_tag
+          tag: '#{tag}'
+          title: #{humanize_tag(tag)}
+          ---
+        HEREDOC
+      end
     end
+  end
 end
---
+++ _tags/jekyll-plugins.md
@@ -0,0 +1,6 @@
+---
+permalink: /tag/jekyll-plugins.html
+layout: page_tag
+tag: 'jekyll-plugins'
+title: jekyll plugins
+---
